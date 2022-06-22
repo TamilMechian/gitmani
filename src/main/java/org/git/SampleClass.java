28feada4c24f3e7e386e244da824046d71e2c10f
TamilMechian <tamizhspa143@gmail.com>
2022-06-22 14:51:58 +0530

    message to rahman

--- src/main/java/org/git/SampleClass.java
+++ src/main/java/org/git/SampleClass.java
@@ -1,5 +1,17 @@
 package org.git;
 
 public class SampleClass {
-
+	
+	private void method1() {
+		System.out.println("No 1 Method");
+	}
+	
+	private void method2() {
+		System.out.println("No 2 Method");
+	}
+	
+	private void method3() {
+		System.out.println("No 3 Method");
+	}
 }
+
